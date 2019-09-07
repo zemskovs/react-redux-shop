6303d8c8563202f428cf8e2e5d7a17ddc4fd1844
Vladimir Zemskov <vlzemskovs@gmail.com>
2019-09-07 13:27:31 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Component } from "react";
+import { render } from "react-dom";
+import { Provider } from 'react-redux'
+import { App } from "./app";
+
+
+render(<App />, document.getElementById("root"));
